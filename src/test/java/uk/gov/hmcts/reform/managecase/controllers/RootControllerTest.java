9904969c7eb492de282700a909d40ef88ad5c56c
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-04-30 10:07:53 +0100

    RDM-8368 - dummy junit test

---
+++ src/test/java/uk/gov/hmcts/reform/managecase/controllers/RootControllerTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.managecase.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RootControllerTest {
+
+    @Test
+    void testNothing() {
+        assertTrue(true);
+    }
+}

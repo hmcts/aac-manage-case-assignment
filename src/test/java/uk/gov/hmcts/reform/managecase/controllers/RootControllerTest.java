ebabfd28d81a0982dda898d7d3b37e3069a8d398
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-04-30 10:12:52 +0100

    RDM-8368 - checkStyle fixes

--- src/test/java/uk/gov/hmcts/reform/managecase/controllers/RootControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/managecase/controllers/RootControllerTest.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.managecase.controllers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RootControllerTest {
 

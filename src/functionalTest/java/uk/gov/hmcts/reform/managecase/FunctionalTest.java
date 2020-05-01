87366903384a36036782ff551f2150d953540ee2
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-05-01 15:25:52 +0100

    RDM-8368 - example smoke and function tests

--- src/functionalTest/java/uk/gov/hmcts/reform/managecase/FunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/FunctionalTest.java
@@ -1,14 +1,31 @@
 package uk.gov.hmcts.reform.managecase;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.restassured.RestAssured.given;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 public class FunctionalTest {
 
+    @Value("${TEST_URL:http://localhost:4454}")
+    private String testUrl;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = testUrl;
+    }
+
     @Test
-    void dummyFunctionalTest() {
-        boolean result = true;
-        assertTrue(result, "should be true");
+    public void info() {
+        given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, "application/json")
+            .when()
+            .get("/info")
+            .then()
+            .statusCode(200);
     }
 }
--- src/smokeTest/java/uk/gov/hmcts/reform/managecase/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/managecase/SmokeTest.java
@@ -1,14 +1,32 @@
 package uk.gov.hmcts.reform.managecase;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.restassured.RestAssured.given;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 public class SmokeTest {
 
-    @Test
-    void dummySmokeTest() {
-        boolean result = true;
-        assertTrue(result, "should be true");
+    @Value("${TEST_URL:http://localhost:4454}")
+    private String testUrl;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = testUrl;
     }
+
+    @Test
+    public void info() {
+        given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, "application/json")
+            .when()
+            .get("/info")
+            .then()
+            .statusCode(200);
+    }
+
 }

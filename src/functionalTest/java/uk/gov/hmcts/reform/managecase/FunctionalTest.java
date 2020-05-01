debcd618f887f1a42a3733fbf4aa40de5e80c0a9
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-05-01 10:31:06 +0100

    RDM-8368 - dummy smoke and functional tests

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/FunctionalTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.managecase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FunctionalTest {
+
+    @Test
+    void dummyFunctionalTest() {
+        boolean result = true;
+        assertTrue(result, "should be true");
+    }
+}
---
+++ src/smokeTest/java/uk/gov/hmcts/reform/managecase/SmokeTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.managecase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTest {
+
+    @Test
+    void dummySmokeTest() {
+        boolean result = true;
+        assertTrue(result, "should be true");
+    }
+}

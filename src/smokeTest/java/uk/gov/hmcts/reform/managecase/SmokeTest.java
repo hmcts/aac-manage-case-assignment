ca065f5af64b14334df98c0695db5800e00df345
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-05-01 15:44:15 +0100

    RDM-8368 - PMD test fixes

--- src/smokeTest/java/uk/gov/hmcts/reform/managecase/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/managecase/SmokeTest.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.managecase;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 public class SmokeTest {
@@ -14,19 +16,18 @@ public class SmokeTest {
     private String testUrl;
 
     @BeforeEach
-    public void setup() {
+    public void setUp() {
         RestAssured.baseURI = testUrl;
     }
 
     @Test
     public void info() {
-        given()
+        Response response = given()
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, "application/json")
             .when()
-            .get("/info")
-            .then()
-            .statusCode(200);
+            .get("/info");
+        assertThat(response.statusCode()).isEqualTo(200);
     }
 
 }

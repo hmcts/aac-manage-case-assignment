0c1af71eaae0fd98127e944bcbf538c45bb57250
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-05-13 11:55:46 +0100

    RDM-8368 - springfox ui fixes

--- src/main/java/uk/gov/hmcts/reform/managecase/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/config/SwaggerConfiguration.java
@@ -2,8 +2,11 @@ package uk.gov.hmcts.reform.managecase.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +23,19 @@ public class SwaggerConfiguration {
             .select()
             .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
             .paths(PathSelectors.any())
-            .build();
+            .build()
+            .apiInfo(apiInfo());
     }
 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Mange case assignment API")
+            .description("Mange case access")
+            .version("1.0.0")
+            .contact(new Contact("CDM",
+                "https://tools.hmcts.net/confluence/display/RCCD/Reform%3A+Core+Case+Data+Home",
+                "corecasedatateam@hmcts.net"))
+            .termsOfServiceUrl("")
+            .build();
+    }
 }

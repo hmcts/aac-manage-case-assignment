562aede67ee1ad70af55f3ce64e39b4d3b4b0dd9
mutlu <mutlu.sancaktutar@hmcts.net>
2020-06-22 21:31:45 +0100

    fix style

--- src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.managecase.befta;
 
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
+
 public class ManageCaseAssignmentTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
     @Override

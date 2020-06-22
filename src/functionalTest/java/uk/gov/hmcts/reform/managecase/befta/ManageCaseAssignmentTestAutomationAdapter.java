226fd6d8c38e39e92f774f4c32ec7de73d076636
mutlu <mutlu.sancaktutar@hmcts.net>
2020-06-22 21:26:25 +0100

    disable role and definition setup

--- src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
@@ -1,16 +1,12 @@
 package uk.gov.hmcts.reform.managecase.befta;
 
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
-import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
-
 public class ManageCaseAssignmentTestAutomationAdapter extends DefaultTestAutomationAdapter {
-
-    private final TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);
 
     @Override
     public void doLoadTestData() {
-        loader.addCcdRoles();
-        loader.importDefinitions();
+        // loader.addCcdRoles();
+        // loader.importDefinitions();
     }
 
 }

bc1f8890138c41816dfcad08fc117d62ce84c568
mutlu <mutlu.sancaktutar@hmcts.net>
2020-06-22 20:52:37 +0100

    introduce runner class

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentsBeftaRunner.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.managecase.befta;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import uk.gov.hmcts.befta.BeftaMain;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "json:target/cucumber.json",
+    glue = "uk.gov.hmcts.befta.player",
+        features = { "classpath:features" }, tags = { "not @Ignore" })
+public class ManageCaseAssignmentsBeftaRunner {
+
+    private ManageCaseAssignmentsBeftaRunner() {
+        // Hide Utility Class Constructor : Utility classes should not have a public or default constructor (squid:S1118)
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        BeftaMain.setUp(new ManageCaseAssignmentTestAutomationAdapter());
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        BeftaMain.tearDown();
+    }
+
+}

77b8b2975547a5541f2aacc79c98855298911fb7
Dan Lysiak <daniel.lysiak@cgi.com>
2020-06-26 15:15:11 +0100

    pmd

--- src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentScenarioPlayer.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentScenarioPlayer.java
@@ -5,11 +5,7 @@ import io.cucumber.java.en.Given;
 public class ManageCaseAssignmentScenarioPlayer {
 
     @Given("logstash has finished indexing case data")
-    public void waitForLogstashToIndexCaseData() {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void waitForLogstashToIndexCaseData() throws InterruptedException {
+        Thread.sleep(5000);
     }
 }

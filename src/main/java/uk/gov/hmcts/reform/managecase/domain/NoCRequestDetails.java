6597e2a2ffc9c27c59c2a08c337ff229ff4751d3
Dan Lysiak <daniel.lysiak@cgi.com>
2020-10-01 10:57:24 +0100

    Remove unused method for GetQuestions API

--- src/main/java/uk/gov/hmcts/reform/managecase/domain/NoCRequestDetails.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/domain/NoCRequestDetails.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.managecase.domain;
 
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.managecase.api.payload.VerifyNoCAnswersResponse;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.CaseViewResource;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.elasticsearch.SearchResultViewItem;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeQuestionsResult;
@@ -15,8 +14,4 @@ public class NoCRequestDetails {
     private OrganisationPolicy organisationPolicy;
     private SearchResultViewItem searchResultViewItem;
     private ChallengeQuestionsResult challengeQuestionsResult;
-
-    public VerifyNoCAnswersResponse toVerifyNoCAnswersResponse(String status) {
-        return new VerifyNoCAnswersResponse(status, getOrganisationPolicy().getOrganisation());
-    }
 }

dfc7e13bcff639e207392e4831d5557e3886b264
Gareth Sullivan <gareth.sullivan@cgi.com>
2020-10-06 18:08:10 +0100

    ACA-68 - Fix merge issues

--- src/test/java/uk/gov/hmcts/reform/managecase/service/NoticeOfChangeServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/managecase/service/NoticeOfChangeServiceTest.java
@@ -13,8 +13,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.mock.web.MockHttpServletRequest;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+import uk.gov.hmcts.reform.managecase.api.errorhandling.CaseCouldNotBeFetchedException;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.AuditEvent;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.CaseViewEvent;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.CaseViewJurisdiction;
@@ -27,12 +27,12 @@ import uk.gov.hmcts.reform.managecase.client.datastore.model.elasticsearch.Heade
 import uk.gov.hmcts.reform.managecase.client.datastore.model.elasticsearch.SearchResultViewHeader;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.elasticsearch.SearchResultViewHeaderGroup;
 import uk.gov.hmcts.reform.managecase.client.datastore.model.elasticsearch.SearchResultViewItem;
+import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeAnswer;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeQuestion;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeQuestionsResult;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.FieldType;
 import uk.gov.hmcts.reform.managecase.repository.DataStoreRepository;
 import uk.gov.hmcts.reform.managecase.repository.DefinitionStoreRepository;
-import uk.gov.hmcts.reform.managecase.repository.IdamRepository;
 import uk.gov.hmcts.reform.managecase.repository.PrdRepository;
 import uk.gov.hmcts.reform.managecase.security.SecurityUtils;
 import uk.gov.hmcts.reform.managecase.util.JacksonUtils;
@@ -44,13 +44,13 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static uk.gov.hmcts.reform.managecase.client.datastore.model.FieldTypeDefinition.PREDEFINED_COMPLEX_CHANGE_ORGANISATION_REQUEST;
 import static uk.gov.hmcts.reform.managecase.client.datastore.model.FieldTypeDefinition.PREDEFINED_COMPLEX_ORGANISATION_POLICY;
 
-@SuppressWarnings({"PMD.UseConcurrentHashMap"})
+@SuppressWarnings({"PMD.UseConcurrentHashMap", "PMD.AvoidDuplicateLiterals"})
 class NoticeOfChangeServiceTest {
 
     private static final String CASE_TYPE_ID = "TEST_CASE_TYPE";
@@ -59,6 +59,7 @@ class NoticeOfChangeServiceTest {
     private static final String CASE_TYPE = "CaseTypeA";
     private static final String QUESTION_TEXT = "QuestionText1";
     private static final String FIELD_ID = "Number";
+    private static final String CHANGE_ORG = "changeOrg";
     private static final String CHALLENGE_QUESTION = "NoC";
     private static final String ANSWER_FIELD = "${applicant.individual.fullname}|${applicant.company.name}|"
         + "${applicant.soletrader.name}:Applicant,${respondent.individual.fullname}|${respondent.company.name}"
@@ -81,8 +82,6 @@ class NoticeOfChangeServiceTest {
     private DataStoreRepository dataStoreRepository;
     @Mock
     private DefinitionStoreRepository definitionStoreRepository;
-    @Mock
-    private IdamRepository idamRepository;
     @Mock
     private PrdRepository prdRepository;
     @Mock
@@ -131,7 +130,7 @@ class NoticeOfChangeServiceTest {
             FieldTypeDefinition fieldTypeDefinition = new FieldTypeDefinition();
             fieldTypeDefinition.setType(PREDEFINED_COMPLEX_CHANGE_ORGANISATION_REQUEST);
             searchResultViewHeader.setCaseFieldTypeDefinition(fieldTypeDefinition);
-            searchResultViewHeader.setCaseFieldId("changeOrg");
+            searchResultViewHeader.setCaseFieldId(CHANGE_ORG);
             caseFields.put(DATE_FIELD, new TextNode("2020-10-01"));
             caseFields.put(DATETIME_FIELD, new TextNode("1985-12-30"));
             caseFields.put(TEXT_FIELD, new TextNode("Text Value"));
@@ -164,10 +163,6 @@ class NoticeOfChangeServiceTest {
             given(dataStoreRepository.findCaseBy(CASE_TYPE_ID, null, CASE_ID)).willReturn(resource);
 
             //Challenge Questions
-            ChallengeQuestion challengeQuestion = new ChallengeQuestion();
-            challengeQuestion.setCaseTypeId(CASE_TYPE_ID);
-            challengeQuestion.setOrder(1);
-            challengeQuestion.setQuestionText(QUESTION_TEXT);
             FieldType fieldType = new FieldType();
             fieldType.setId(FIELD_ID);
             fieldType.setType(FIELD_ID);
@@ -186,13 +181,9 @@ class NoticeOfChangeServiceTest {
             given(definitionStoreRepository.challengeQuestions(CASE_TYPE_ID, "NoCChallenge"))
                 .willReturn(challengeQuestionsResult);
 
-            MockHttpServletRequest request = new MockHttpServletRequest("GET", "/url");
-            RequestContext context = new RequestContext();
-            context.setRequest(request);
-            RequestContext.testSetCurrentContext(context);
-
-            UserInfo userInfo = new UserInfo("","","", "", "", Arrays.asList("pui-caa"));
-            given(idamRepository.getUserInfo(context.getRequest().getAuthType())).willReturn(userInfo);
+            UserInfo userInfo = new UserInfo("","","", "", "",
+                                             Arrays.asList("pui-caa"));
+            given(securityUtils.getUserInfo()).willReturn(userInfo);
         }
 
         @Test
@@ -216,7 +207,7 @@ class NoticeOfChangeServiceTest {
             FieldTypeDefinition fieldTypeDefinition = new FieldTypeDefinition();
             fieldTypeDefinition.setType(PREDEFINED_COMPLEX_CHANGE_ORGANISATION_REQUEST);
             searchResultViewHeader.setCaseFieldTypeDefinition(fieldTypeDefinition);
-            searchResultViewHeader.setCaseFieldId("changeOrg");
+            searchResultViewHeader.setCaseFieldId(CHANGE_ORG);
 
             caseFields.put(DATE_FIELD, new TextNode("2020-10-01"));
             caseFields.put(DATETIME_FIELD, new TextNode("1985-12-30"));
@@ -228,7 +219,7 @@ class NoticeOfChangeServiceTest {
                                                       + "                    \"CaseRoleId\": \"role\"\n"
                                                       + "                }\n"
                                                       + "            }", JsonNode.class);
-            caseFields.put("changeOrg", actualObj);
+            caseFields.put(CHANGE_ORG, actualObj);
             SearchResultViewItem item = new SearchResultViewItem("CaseId", caseFields, caseFields);
             viewItems.add(item);
             SearchResultViewHeaderGroup correctHeader = new SearchResultViewHeaderGroup(
@@ -316,13 +307,9 @@ class NoticeOfChangeServiceTest {
         @DisplayName("Must return an error if there is not an Organisation Policy field containing a "
             + "case role for each set of answers")
         void shouldThrowErrorMissingRoleInOrgPolicy() {
-            ChallengeQuestion challengeQuestion = new ChallengeQuestion();
-            challengeQuestion.setCaseTypeId(CASE_TYPE_ID);
-            challengeQuestion.setOrder(1);
-            challengeQuestion.setQuestionText(QUESTION_TEXT);
             FieldType fieldType = new FieldType();
-            fieldType.setId("Number");
-            fieldType.setType("Number");
+            fieldType.setId(FIELD_ID);
+            fieldType.setType(FIELD_ID);
             fieldType.setMin(null);
             fieldType.setMax(null);
             ChallengeAnswer challengeAnswer = new ChallengeAnswer(ANSWER_FIELD_RESPONDENT);
@@ -360,12 +347,9 @@ class NoticeOfChangeServiceTest {
         @DisplayName(" must return an error response when the solicitor does not have access to the "
             + "jurisdiction of the case")
         void shouldThrowErrorInsufficientPrivilegesForSolicitor() {
-            MockHttpServletRequest request = new MockHttpServletRequest("GET", "/url");
-            RequestContext context = new RequestContext();
-            context.setRequest(request);
-            RequestContext.testSetCurrentContext(context);
-            UserInfo userInfo = new UserInfo("","","", "", "", Arrays.asList("caseworker-JurisdictionA-solicitor"));
-            given(idamRepository.getUserInfo(context.getRequest().getAuthType())).willReturn(userInfo);
+            UserInfo userInfo = new UserInfo("","","", "", "",
+                                             Arrays.asList("caseworker-JurisdictionA-solicitor"));
+            given(securityUtils.getUserInfo()).willReturn(userInfo);
 
             assertThatThrownBy(() -> service.getChallengeQuestions(CASE_ID))
                 .isInstanceOf(ValidationException.class)

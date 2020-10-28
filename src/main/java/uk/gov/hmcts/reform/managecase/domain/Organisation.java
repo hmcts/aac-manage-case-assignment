0f1bcf20c3816eebaccdbd1e7525ab8592efa0ff
Dan Lysiak <daniel.lysiak@cgi.com>
2020-10-28 12:29:49 +0000

    Add @NoArgsConstructor

--- src/main/java/uk/gov/hmcts/reform/managecase/domain/Organisation.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/domain/Organisation.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.managecase.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Organisation {
 
     @JsonProperty("OrganisationID")

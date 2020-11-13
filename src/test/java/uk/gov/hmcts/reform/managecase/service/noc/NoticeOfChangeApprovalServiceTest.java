a342fc504ac848b59809b9d9053fb94b45d17aa3
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-11-13 14:53:50 +0000

    ACA-85 moved to correct package.

--- src/test/java/uk/gov/hmcts/reform/managecase/service/noc/NoticeOfChangeApprovalServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/managecase/service/noc/NoticeOfChangeApprovalServiceTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.managecase.service;
+package uk.gov.hmcts.reform.managecase.service.noc;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.BeforeEach;

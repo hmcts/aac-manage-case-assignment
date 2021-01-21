b4b86031ad48541681ba1f42f478f8e7736dc141
tomxelliott <tomelliott@Toms-MBP.home>
2021-01-21 18:02:45 +0000

    RDM-10876: Fix checkstyle errors

--- src/main/java/uk/gov/hmcts/reform/managecase/repository/DefaultDefinitionStoreRepository.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/repository/DefaultDefinitionStoreRepository.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.managecase.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.DefinitionStoreApiClient;
@@ -25,7 +24,7 @@ public class DefaultDefinitionStoreRepository implements DefinitionStoreReposito
     }
 
     @Override
-    @Cacheable(value = "challengeQuestions",  key="#caseTypeId + #root.target.versions[#caseTypeId]",
+    @Cacheable(value = "challengeQuestions", key = "#caseTypeId + #root.target.versions[#caseTypeId]",
         condition = "#root.target.getLatestVersion(#caseTypeId) == #root.target.versions[#caseTypeId]")
     public ChallengeQuestionsResult challengeQuestions(String caseTypeId, String challengeQuestionId) {
         versions.put(caseTypeId, getLatestVersion(caseTypeId));
@@ -33,7 +32,7 @@ public class DefaultDefinitionStoreRepository implements DefinitionStoreReposito
     }
 
     @Override
-    @Cacheable(value = "caseRoles", key="#caseTypeId + #root.target.versions[#caseTypeId]",
+    @Cacheable(value = "caseRoles", key = "#caseTypeId + #root.target.versions[#caseTypeId]",
         condition = "#root.target.getLatestVersion(#caseTypeId) == #root.target.versions[#caseTypeId]")
     public List<CaseRole> caseRoles(String userId, String jurisdiction, String caseTypeId) {
         versions.put(caseTypeId, getLatestVersion(caseTypeId));

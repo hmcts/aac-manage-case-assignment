8cb30668c4db78fd75143897527274e6348542ed
ashleynoronha <ashleydeannoronha@gmail.com>
2021-03-12 09:26:33 +0000

    remove unused import

--- src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
@@ -13,8 +13,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Optional.ofNullable;
-
 public class ManageCaseAssignmentTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
     private final TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);

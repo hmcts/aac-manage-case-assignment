a373d777dd1a77fdde98775ae451f06096842692
padmapriyanalam <padmapriya.nalam@gmail.com>
2021-07-23 16:05:42 +0100

    ACA-142 Added exception handler

--- src/main/java/uk/gov/hmcts/reform/managecase/api/errorhandling/BadRequestException.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/api/errorhandling/BadRequestException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class BadRequestException extends ApiException {
+public class BadRequestException extends CaseAssignedUserRoleException {
     public BadRequestException(final String message) {
         super(message);
     }
--- src/main/java/uk/gov/hmcts/reform/managecase/api/errorhandling/CaseRoleAccessException.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/api/errorhandling/CaseRoleAccessException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.FORBIDDEN)
-public class CaseRoleAccessException extends ApiException {
+public class CaseRoleAccessException extends CaseAssignedUserRoleException {
 
     public CaseRoleAccessException(String message) {
         super(message);

5969834340ee1758a34fdb79160a88c359a7ab0b
Mike Brown <michael.brown@hmcts.net>
2022-08-19 11:50:42 +0100

    CCD-3509 CVE-2021-22044 webEndpointServletHandlerMapping removed from TestIdamConfiguration maybe not needed

--- src/test/java/uk/gov/hmcts/reform/managecase/TestIdamConfiguration.java
+++ src/test/java/uk/gov/hmcts/reform/managecase/TestIdamConfiguration.java
@@ -1,30 +1,12 @@
 package uk.gov.hmcts.reform.managecase;
 
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
-import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
-import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
-import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
-import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
-import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
-import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.util.StringUtils;
 import org.springframework.web.context.ContextCleanupListener;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Configuration
 public class TestIdamConfiguration extends ContextCleanupListener {
@@ -49,35 +31,5 @@ public class TestIdamConfiguration extends ContextCleanupListener {
             .clientId("client-id")
             .clientSecret("client-secret")
             .build();
-    }
-
-    //CCD-3509 CVE-2021-22044 required to fix null pointers in integration tests,
-    //conflict in Springfox after Springboot 2.6.10
-    @Bean
-    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
-        ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
-        EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
-        WebEndpointProperties webEndpointProperties, Environment environment) {
-
-        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
-        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-        allEndpoints.addAll(webEndpoints);
-        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-        String basePath = webEndpointProperties.getBasePath();
-        EndpointMapping endpointMapping = new EndpointMapping(basePath);
-        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
-                                                                             basePath);
-        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
-                                                corsProperties.toCorsConfiguration(),
-                                                new EndpointLinksResolver(allEndpoints, basePath),
-                                                shouldRegisterLinksMapping, null);
-    }
-
-
-    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
-                                               String basePath) {
-        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
-            || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }

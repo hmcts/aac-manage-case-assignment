bf6707a76380b33159cedc681cd5927db9fc4e1d
marianadpereira <mariana.pereira@hmcts.net>
2022-12-01 09:38:23 +0000

    importing def file like data-store

--- src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/managecase/befta/ManageCaseAssignmentTestAutomationAdapter.java
@@ -2,12 +2,10 @@ package uk.gov.hmcts.reform.managecase.befta;
 
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
-import uk.gov.hmcts.befta.DefaultBeftaTestDataLoader;
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.exception.FunctionalTestException;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
 import uk.gov.hmcts.befta.util.ReflectionUtils;
-import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
-import uk.gov.hmcts.befta.BeftaMain;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,17 +14,10 @@ import java.util.Map;
 
 public class ManageCaseAssignmentTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
-    private TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this,
-                                                                                         "uk/gov/hmcts/ccd/test_definitions/valid", BeftaMain.getConfig().getDefinitionStoreUrl());
-
     @Override
-    public BeftaTestDataLoader getDataLoader() {
-        return new DefaultBeftaTestDataLoader() {
-            @Override
-            protected void doLoadTestData() {
-                loader.addCcdRoles();
-                loader.importDefinitions();
-            }
+    protected BeftaTestDataLoader buildTestDataLoader() {
+        return new DataLoaderToDefinitionStore(this,
+                                               DataLoaderToDefinitionStore.VALID_CCD_TEST_DEFINITIONS_PATH) {
         };
     }
 

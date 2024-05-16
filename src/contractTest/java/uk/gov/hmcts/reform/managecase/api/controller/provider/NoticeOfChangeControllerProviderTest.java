ea3a23fbdc21c91411534e512867dc9eedbe3034
Adam <adam.lee2@justice.gov.uk>
2024-05-16 15:03:12 +0100

    Add contract config and setup state

--- src/contractTest/java/uk/gov/hmcts/reform/managecase/api/controller/provider/NoticeOfChangeControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/managecase/api/controller/provider/NoticeOfChangeControllerProviderTest.java
@@ -16,18 +16,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.managecase.api.controller.NoticeOfChangeController;
 import uk.gov.hmcts.reform.managecase.client.datastore.CaseUserRole;
+import uk.gov.hmcts.reform.managecase.client.prd.FindOrganisationResponse;
 import uk.gov.hmcts.reform.managecase.config.MapperConfig;
 import uk.gov.hmcts.reform.managecase.repository.DataStoreRepository;
-import uk.gov.hmcts.reform.managecase.repository.IdamRepository;
 import uk.gov.hmcts.reform.managecase.repository.PrdRepository;
-import uk.gov.hmcts.reform.managecase.security.SecurityUtils;
-import uk.gov.hmcts.reform.managecase.util.JacksonUtils;
 
 import java.io.IOException;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.eq;
+import static java.util.Collections.emptyList;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.managecase.TestFixtures.ProfessionalUserFixture.user;
+import static uk.gov.hmcts.reform.managecase.TestFixtures.ProfessionalUserFixture.usersByOrganisation;
 
 @ExtendWith(SpringExtension.class)
 @Provider("acc_manageCaseAssignment")
@@ -42,27 +44,20 @@ import static org.mockito.BDDMockito.given;
 @IgnoreNoPactsToVerify
 public class NoticeOfChangeControllerProviderTest {
 
-    private static final String ORGANIZATION_ID = "TEST_ORG";
+    private static final String TEST_APP_ORG_ID = "appOrgId";
+    private static final String TEST_APP_ORG_NAME = "appOrgName";
     private static final String ASSIGNEE_ID = "0a5874a4-3f38-4bbd-ba4c";
-    private static final String BEAR_TOKEN = "TestBearToken";
-
     private static final String ASSIGNEE_ID2 = "38130f09-0010-4c12-afd1-2563bb25d1d3";
-    private static final String ASSIGNEE_ID3 = "userId";
-    private static final String CASE_ID = "12345678";
+    private static final String CASE_ID = "1583841721773828";
     private static final String CASE_ID2 = "87654321";
     private static final String CASE_ROLE = "[CR1]";
     private static final String CASE_ROLE2 = "[CR2]";
 
     @Autowired
     DataStoreRepository dataStoreRepository;
+
     @Autowired
     PrdRepository prdRepository;
-    @Autowired
-    IdamRepository idamRepository;
-    @Autowired
-    JacksonUtils jacksonUtils;
-    @Autowired
-    SecurityUtils securityUtils;
 
     @Autowired
     NoticeOfChangeController noticeOfChangeController;
@@ -87,11 +82,16 @@ public class NoticeOfChangeControllerProviderTest {
 
     @State({"A notice of change against case"})
     public void toApplyNoticeOfChange() throws IOException {
-        given(dataStoreRepository.getCaseAssignments(eq(List.of(CASE_ID, CASE_ID2)),
-                                                     eq(List.of(ASSIGNEE_ID, ASSIGNEE_ID2))))
+        given(dataStoreRepository.getCaseAssignments(any(), any()))
             .willReturn(List.of(
                 new CaseUserRole(CASE_ID, ASSIGNEE_ID, CASE_ROLE),
-                new CaseUserRole(CASE_ID2, ASSIGNEE_ID2, CASE_ROLE),
-                new CaseUserRole(CASE_ID2, ASSIGNEE_ID2, CASE_ROLE2)));
+                new CaseUserRole(CASE_ID2, ASSIGNEE_ID2, CASE_ROLE2)
+            ));
+
+        given(prdRepository.findUsersByOrganisation(anyString()))
+            .willReturn(usersByOrganisation(user(ASSIGNEE_ID), user(ASSIGNEE_ID2)));
+
+        when(prdRepository.findOrganisationAddress(any()))
+            .thenReturn(new FindOrganisationResponse(emptyList(), TEST_APP_ORG_ID, TEST_APP_ORG_NAME));
     }
 }

99900981bc644ad1a9c4dbf22f36dde6ea902527
JamesCollettCGI <james.collett@hmcts.net>
2024-10-15 17:17:27 +0100

    NocProviderTest.java and NocRestController.java version one

--- src/contractTest/java/uk/gov/hmcts/reform/managecase/pactprovider/noc/NocProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/managecase/pactprovider/noc/NocProviderTest.java
@@ -6,7 +6,6 @@ import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,42 +13,31 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.managecase.api.controller.NoticeOfChangeController;
+import uk.gov.hmcts.reform.managecase.api.payload.RequestNoticeOfChangeResponse;
+import uk.gov.hmcts.reform.managecase.api.payload.VerifyNoCAnswersRequest;
+import uk.gov.hmcts.reform.managecase.api.payload.VerifyNoCAnswersResponse;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeQuestion;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeQuestionsResult;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.FieldType;
+import uk.gov.hmcts.reform.managecase.domain.ApprovalStatus;
+import uk.gov.hmcts.reform.managecase.domain.NoCRequestDetails;
+import uk.gov.hmcts.reform.managecase.domain.Organisation;
 import uk.gov.hmcts.reform.managecase.pactprovider.noc.controller.NocRestController;
 import uk.gov.hmcts.reform.managecase.service.noc.NoticeOfChangeQuestions;
-import uk.gov.hmcts.reform.managecase.service.noc.NoticeOfChangeApprovalService;
-import uk.gov.hmcts.reform.managecase.service.noc.VerifyNoCAnswersService;
-import uk.gov.hmcts.reform.managecase.service.noc.PrepareNoCService;
 import uk.gov.hmcts.reform.managecase.service.noc.RequestNoticeOfChangeService;
-import uk.gov.hmcts.reform.managecase.service.noc.ApplyNoCDecisionService;
-import uk.gov.hmcts.reform.managecase.util.JacksonUtils;
-
+import uk.gov.hmcts.reform.managecase.service.noc.VerifyNoCAnswersService;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 /*
- * **** WORK IN PROGRESS ****
- * Three ways of running this Provider Test :-
- * [1] Set useProxyRestController = false                       -- Uses actual Rest Controller ,
- *                                                                 passes with changes to JSON below.
- * [2] Set useProxyRestController = true and activate method 1  -- Uses proxy  Rest Controller  ,  passes.
- * [3] Set useProxyRestController = true and activate method 2  -- Uses proxy  Rest Controller  ,
- *                                                                 passes with changes to JSON below.
- *
- * The PactFolder JSON file currently only contains ONE test (providerState "a case with id 1234567890123456 exist").
- *                                                  ^^^
- * Changes made to JSON file :-
- *     Updated order from "1" to 1                    -- reasonable because ChallengeQuestion order is Integer.
- *     Updated min & max from 0 & 10 to "0" & "10"    -- reasonable because FieldType min & max are String.
+ * CHANGES MAKE TO JSON FILE "INTERACTION 1" :-
+ *     Updated order from "1" to 1                    -- because ChallengeQuestion order is Integer.
+ *     Updated min & max from 0 & 10 to "0" & "10"    -- because FieldType min & max are String.
  *     Removed collection_field_type  (temporarily?)  -- because FieldType collectionFieldType is NOT String.
- *
- * Next thing to investigate:  LuhnCheck
  */
 
 @Provider("acc_manageCaseAssignment_Noc")
@@ -57,34 +45,27 @@ import static org.mockito.Mockito.when;
     url = "${PACT_BROKER_FULL_URL:http://localhost:9292}",
     consumerVersionSelectors = {@VersionSelector(tag = "master")})
 @IgnoreNoPactsToVerify
-@PactFolder("target/pacts/reform/acc_manageCaseAssignment_noc")
+//@PactFolder("target/pacts/reform/acc_manageCaseAssignment_noc")
 @ExtendWith(SpringExtension.class)
 public class NocProviderTest {
 
-    @Mock private NoticeOfChangeQuestions mockNoticeOfChangeQuestions;
-    @Mock private NoticeOfChangeApprovalService mockNoticeOfChangeApprovalService;
-    @Mock private VerifyNoCAnswersService mockVerifyNoCAnswersService;
-    @Mock private PrepareNoCService mockPrepareNoCService;
-    @Mock private RequestNoticeOfChangeService mockRequestNoticeOfChangeService;
-    @Mock private ApplyNoCDecisionService mockApplyNoCDecisionService;
-    @Mock private JacksonUtils mockJacksonUtils;
+    @Mock
+    private NoticeOfChangeQuestions mockNoticeOfChangeQuestions;
 
-    private boolean useProxyRestController = true;
+    @Mock
+    private VerifyNoCAnswersService mockVerifyNoCAnswersService;
+
+    @Mock
+    private RequestNoticeOfChangeService mockRequestNoticeOfChangeService;
+
+    @Mock
+    private NoCRequestDetails mockNoCRequestDetails;
 
     @BeforeEach
     void beforeCreate(PactVerificationContext context) {
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
-        if (useProxyRestController) {
-            // Either use PROXY Rest Controller :-
-            testTarget.setControllers(new NocRestController(mockNoticeOfChangeQuestions));
-        } else {
-            // Or use ACTUAL Rest Controller :-
-            testTarget.setControllers(new NoticeOfChangeController(mockNoticeOfChangeQuestions,
-                                                                   mockNoticeOfChangeApprovalService,
-                                                                   mockVerifyNoCAnswersService, mockPrepareNoCService,
-                                                                   mockRequestNoticeOfChangeService,
-                                                                   mockApplyNoCDecisionService, mockJacksonUtils));
-        }
+        testTarget.setControllers(new NocRestController(mockNoticeOfChangeQuestions, mockVerifyNoCAnswersService,
+                                                        mockRequestNoticeOfChangeService));
         if (context != null) {
             context.setTarget(testTarget);
         }
@@ -98,18 +79,44 @@ public class NocProviderTest {
         }
     }
 
-    // Mock the interaction.
     @State("a case with id 1234567890123456 exists")
     public void getNoticeOfChangeQuestions1() {
         when(mockNoticeOfChangeQuestions.getChallengeQuestions(anyString())).thenReturn(mockChallengeQuestionsResult());
     }
 
-    // Mock the interaction.
     @State("a case with an error exists")
     public void getNoticeOfChangeQuestions2() {
     }
 
-    public ChallengeQuestionsResult mockChallengeQuestionsResult() {
+    @State("A valid submit NoC event is requested")
+    public void requestNoticeOfChange1() {
+        when(mockVerifyNoCAnswersService.verifyNoCAnswers(any(VerifyNoCAnswersRequest.class)))
+            .thenReturn(mockNoCRequestDetails);
+        when(mockRequestNoticeOfChangeService.requestNoticeOfChange(any(NoCRequestDetails.class)))
+            .thenReturn(mockRequestNoticeOfChangeResponse());
+    }
+
+    @State("A NoC answer request with invalid case ID")
+    public void requestNoticeOfChange2() {
+        when(mockVerifyNoCAnswersService.verifyNoCAnswers(any(VerifyNoCAnswersRequest.class)))
+            .thenReturn(mockNoCRequestDetails);
+    }
+
+    @State("A valid NoC answers verification request")
+    public void verifyNoticeOfChangeAnswers1() {
+        when(mockVerifyNoCAnswersService.verifyNoCAnswers(any(VerifyNoCAnswersRequest.class)))
+            .thenReturn(mockNoCRequestDetails);
+        when(mockNoCRequestDetails.toVerifyNoCAnswersResponse(anyString())).thenReturn(mockVerifyNoCAnswersResponse());
+    }
+
+    @State("An invalid NoC answer request")
+    public void verifyNoticeOfChangeAnswers2() {
+        when(mockVerifyNoCAnswersService.verifyNoCAnswers(any(VerifyNoCAnswersRequest.class)))
+            .thenReturn(mockNoCRequestDetails);
+    }
+
+
+    private ChallengeQuestionsResult mockChallengeQuestionsResult() {
         FieldType answerFieldType = FieldType.builder()
             .id("Email")
             .type("Email")
@@ -133,5 +140,18 @@ public class NocProviderTest {
         challengeQuestionsResponse.add(challengeQuestion);
 
         return ChallengeQuestionsResult.builder().questions(challengeQuestionsResponse).build();
+    }
+
+    private RequestNoticeOfChangeResponse mockRequestNoticeOfChangeResponse() {
+        return RequestNoticeOfChangeResponse.builder()
+            .approvalStatus(ApprovalStatus.APPROVED)
+            .caseRole("[Claimant]")
+            .status("Notice of request has been successfully submitted.")
+            .build();
+    }
+
+    private VerifyNoCAnswersResponse mockVerifyNoCAnswersResponse() {
+        return new VerifyNoCAnswersResponse("Notice of Change answers verified successfully",
+                                            new Organisation("QUK822NA", "Some Org"));
     }
 }
--- src/contractTest/java/uk/gov/hmcts/reform/managecase/pactprovider/noc/controller/NocRestController.java
+++ src/contractTest/java/uk/gov/hmcts/reform/managecase/pactprovider/noc/controller/NocRestController.java
@@ -1,10 +1,5 @@
 package uk.gov.hmcts.reform.managecase.pactprovider.noc.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Example;
-import io.swagger.annotations.ExampleProperty;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -13,20 +8,21 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.hibernate.validator.constraints.LuhnCheck;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import uk.gov.hmcts.reform.managecase.api.errorhandling.AuthError;
 import uk.gov.hmcts.reform.managecase.api.errorhandling.noc.NoCValidationError;
 import uk.gov.hmcts.reform.managecase.api.payload.RequestNoticeOfChangeRequest;
+import uk.gov.hmcts.reform.managecase.api.payload.RequestNoticeOfChangeResponse;
 import uk.gov.hmcts.reform.managecase.api.payload.VerifyNoCAnswersRequest;
+import uk.gov.hmcts.reform.managecase.api.payload.VerifyNoCAnswersResponse;
 import uk.gov.hmcts.reform.managecase.client.definitionstore.model.ChallengeQuestionsResult;
+import uk.gov.hmcts.reform.managecase.domain.NoCRequestDetails;
 import uk.gov.hmcts.reform.managecase.service.noc.NoticeOfChangeQuestions;
+import uk.gov.hmcts.reform.managecase.service.noc.RequestNoticeOfChangeService;
+import uk.gov.hmcts.reform.managecase.service.noc.VerifyNoCAnswersService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -34,120 +30,27 @@ import javax.validation.constraints.Size;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-// **** WORK IN PROGRESS ****
-// See NoticeOfChangeController.java
-
 @RestController
-@Validated
-@RequestMapping(path = "/noc")
 public class NocRestController {
 
-    public static final String GET_NOC_QUESTIONS_METHOD_2 = "/noc-questions";               // Uses innjected mock
-    public static final String GET_NOC_QUESTIONS_METHOD_1 = "/noc-questions-DEACTIVATED";   // Uses hard coded JSON
+    private static final String VERIFY_NOC_ANSWERS_MESSAGE = "Notice of Change answers verified successfully";
 
     private final NoticeOfChangeQuestions noticeOfChangeQuestions;
 
-    public NocRestController(final NoticeOfChangeQuestions noticeOfChangeQuestions) {
+    private final VerifyNoCAnswersService verifyNoCAnswersService;
+
+    private final RequestNoticeOfChangeService requestNoticeOfChangeService;
+
+    public NocRestController(final NoticeOfChangeQuestions noticeOfChangeQuestions,
+                             final VerifyNoCAnswersService verifyNoCAnswersService,
+                             final RequestNoticeOfChangeService requestNoticeOfChangeService) {
         this.noticeOfChangeQuestions = noticeOfChangeQuestions;
+        this.verifyNoCAnswersService = verifyNoCAnswersService;
+        this.requestNoticeOfChangeService = requestNoticeOfChangeService;
     }
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     /*
-     * Handle Pact States (using METHOD TWO , injected mock) :-
-     *   "a case with id 1234567890123456 exists"  (response 200)
-     *   "a case with an error exists"             (response 400)
-     */
-    @GetMapping(path = GET_NOC_QUESTIONS_METHOD_2, produces = APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Get Notice of Change questions", notes = "Get Notice of Change questions")
-    @ResponseStatus(HttpStatus.OK)
-    @io.swagger.annotations.ApiResponses({
-        @io.swagger.annotations.ApiResponse(
-            code = 200,
-            message = "Challenge questions returned successfully.",
-            response = ChallengeQuestionsResult.class,
-            examples = @Example({
-                @ExampleProperty(
-                    value = "{\"questions\":["
-                        + "{\"case_type_id\":\"caseType\","
-                        + "\"order\":1,"
-                        + "\"question_text\":\"questionText\","
-                        + "\"answer_field_type\""
-                        + ":{\"id\":\"Number\","
-                        + "\"type\":\"Number\","
-                        + "\"min\":null,"
-                        + "\"max\":null,"
-                        + "\"regular_expression\":null,"
-                        + "\"fixed_list_items\":[],"
-                        + "\"complex_fields\":[],"
-                        + "\"collection_field_type\":null"
-                        + "},"
-                        + "\"display_context_parameter\":null,"
-                        + "\"challenge_question_id\":\"NoC\","
-                        + "\"answer_field\":null,"
-                        + "\"question_id\":\"QuestionId1\"}]}\n",
-                    mediaType = APPLICATION_JSON_VALUE)
-            })),
-        @io.swagger.annotations.ApiResponse(
-            code = 400,
-            message = "One or more of the following reasons:\n"
-                + "1. " + "Case ID can not be empty" + ", \n"
-                + "2. " + "Case ID has to be a valid 16-digit Luhn number" + ", \n"
-                + "3. " + "No NoC events available for this case type" + ", \n"
-                + "4. " + "Multiple NoC Request events found for the user" + ", \n"
-                + "5. " + "More than one change request found on the case" + ", \n"
-                + "6. " + "Ongoing NoC request in progress \n"
-                + "7. " + "Insufficient privileges for notice of change request \n"
-                + "8. " + "No Organisation Policy for one or more of the roles available"
-                + " for the notice of change request \n",
-            examples = @Example({
-                @ExampleProperty(
-                    value = "{\"message\": \"Case ID has to be a valid 16-digit Luhn number\","
-                        + " \"code\": \"case-id-invalid\","
-                        + " \"status\": \"BAD_REQUEST\" }",
-                    mediaType = APPLICATION_JSON_VALUE)
-            })),
-        @io.swagger.annotations.ApiResponse(
-            code = 404,
-            message = "Case could not be found",
-            examples = @Example({
-                @ExampleProperty(
-                    value = "{\"message\": \"Case could not be found\","
-                        + " \"status\": \"NOT_FOUND\" }",
-                    mediaType = APPLICATION_JSON_VALUE)
-            })),
-        @io.swagger.annotations.ApiResponse(
-            code = 401,
-            message = AuthError.AUTHENTICATION_TOKEN_INVALID),
-        @io.swagger.annotations.ApiResponse(
-            code = 403,
-            message = AuthError.UNAUTHORISED_S2S_SERVICE)
-    })
-    public ResponseEntity<String> getNoticeOfChangeQuestionsMethod2(@RequestParam("case_id")
-                                                               @Valid
-                                                               @NotEmpty(message = NoCValidationError.NOC_CASE_ID_EMPTY)
-                                                               @Size(min = 16, max = 16, message =
-                                                                   NoCValidationError.NOC_CASE_ID_INVALID_LENGTH)
-                                                               @LuhnCheck(message =
-                                                                   NoCValidationError.NOC_CASE_ID_INVALID,
-                                                                   ignoreNonDigitCharacters = false)
-                                                               String caseId) throws JsonProcessingException {
-        System.out.println("********** JCDEBUG: METHOD TWO: caseId = " + caseId);
-
-        ChallengeQuestionsResult challengeQuestionsResult = noticeOfChangeQuestions.getChallengeQuestions(caseId);
-        if (challengeQuestionsResult != null) {
-            System.out.println(objectMapper.writeValueAsString(challengeQuestionsResult));
-            return ResponseEntity.status(HttpStatus.OK).body(objectMapper.writeValueAsString(challengeQuestionsResult));
-        } else {
-            String jsonString = "{\"status\":\"BAD_REQUEST\",\"message\":\"Case ID has to be a valid 16-digit Luhn "
-                + "number\",\"code\":\"case-id-invalid\"}";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonString);
-        }
-    }
-
-
-    /*
-     * Handle Pact States (using METHOD ONE , hard coded JSON) :-
+     * Handle Pact States :-
      *   "a case with id 1234567890123456 exists"  (response 200)
      *   "a case with an error exists"             (response 400)
      */
@@ -165,32 +68,22 @@ public class NocRestController {
                 @Content(mediaType = "application/json")
             })
     })
-    @GetMapping(path = GET_NOC_QUESTIONS_METHOD_1, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getNoticeOfChangeQuestionsMethod1(@RequestParam("case_id")
-                                                             @Valid
-                                                             @NotEmpty(message = NoCValidationError.NOC_CASE_ID_EMPTY)
-                                                             @Size(min = 16, max = 16, message =
-                                                                     NoCValidationError.NOC_CASE_ID_INVALID_LENGTH)
-                                                             @LuhnCheck(message =
-                                                                     NoCValidationError.NOC_CASE_ID_INVALID,
-                                                                     ignoreNonDigitCharacters = false)
-                                                                     String caseId) {
-
-        System.out.println("********** JCDEBUG: METHOD ONE: caseId = " + caseId);
-
-        if ("1234567890123456".equals(caseId)) {
-            System.out.println("**** JCDEBUG: Returning OK");
-            String jsonString = "{\"questions\":[{\"case_type_id\":\"Probate\",\"order\":\"1\",\"question_text\":\""
-                + "What is their Email?\",\"answer_field_type\":{\"id\":\"Email\",\"type\":\"Email\",\"min\":0,\"max\":"
-                + "10,\"regular_expression\":\"asdsa\",\"collection_field_type\":\"type\"},\""
-                + "display_context_parameter\":\"1\",\"challenge_question_id\":\"NoC\",\"answer_field\":\"\",\""
-                + "question_id\":\"QuestionId67745\"}]}";
-            return ResponseEntity.status(HttpStatus.OK).body(jsonString);
+    @GetMapping(path = "/noc/noc-questions", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity getNoticeOfChangeQuestions(@RequestParam("case_id")
+                                                     @Valid
+                                                     @NotEmpty(message = NoCValidationError.NOC_CASE_ID_EMPTY)
+                                                     @Size(min = 16, max = 16, message =
+                                                         NoCValidationError.NOC_CASE_ID_INVALID_LENGTH)
+                                                     @LuhnCheck(message =
+                                                         NoCValidationError.NOC_CASE_ID_INVALID,
+                                                         ignoreNonDigitCharacters = false)
+                                                     String caseId) {
+        ChallengeQuestionsResult challengeQuestionsResult = noticeOfChangeQuestions.getChallengeQuestions(caseId);
+        if (challengeQuestionsResult != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(challengeQuestionsResult);
         } else {
-            System.out.println("**** JCDEBUG: Returning BAD_REQUEST");
-            String jsonString = "{\"status\":\"BAD_REQUEST\",\"message\":\"Case ID has to be a valid 16-digit Luhn "
-                + "number\",\"code\":\"case-id-invalid\"}";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonString);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BadRequest("", "", "",
+                                                                                     new String[]{}));
         }
     }
 
@@ -214,13 +107,22 @@ public class NocRestController {
                 @Content(mediaType = "application/json")
             })
     })
-    @PostMapping(path = "/noc-requests", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> requestNoticeOfChange(@Valid @RequestBody RequestNoticeOfChangeRequest
-                                                                requestNoticeOfChangeRequest) {
+    @PostMapping(path = "/noc/noc-requests", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity requestNoticeOfChange(@RequestBody RequestNoticeOfChangeRequest
+                                                requestNoticeOfChangeRequest) {
 
-        String jsonString = "{\"approval_status\":\"APPROVED\",\"case_role\":\"[Claimant]\",\"status_message\":\""
-            + "Notice of request has been successfully submitted.\"}";
-        return ResponseEntity.status(HttpStatus.CREATED).body(jsonString);
+        VerifyNoCAnswersRequest verifyNoCAnswersRequest = new VerifyNoCAnswersRequest(
+            requestNoticeOfChangeRequest.getCaseId(),
+            requestNoticeOfChangeRequest.getAnswers());
+        NoCRequestDetails noCRequestDetails = verifyNoCAnswersService.verifyNoCAnswers(verifyNoCAnswersRequest);
+        RequestNoticeOfChangeResponse requestNoticeOfChangeResponse = requestNoticeOfChangeService
+            .requestNoticeOfChange(noCRequestDetails);
+        if (requestNoticeOfChangeResponse != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(requestNoticeOfChangeResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BadRequest("", "", "",
+                                                                                     new String[]{}));
+        }
     }
 
 
@@ -243,12 +145,17 @@ public class NocRestController {
                 @Content(mediaType = "application/json")
             })
     })
-    @PostMapping(path = "/verify-noc-answers", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> verifyNoticeOfChangeAnswers(@Valid @RequestBody VerifyNoCAnswersRequest
-                                                                      verifyNoCAnswersRequest) {
+    @PostMapping(path = "/noc/verify-noc-answers", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity verifyNoticeOfChangeAnswers(@RequestBody VerifyNoCAnswersRequest verifyNoCAnswersRequest) {
 
-        String jsonString = "{\"organisation\":{\"OrganisationID\":\"QUK822NA\",\"OrganisationName\":\"Some Org\"},\""
-            + "status_message\":\"Notice of Change answers verified successfully\"}";
-        return ResponseEntity.status(HttpStatus.CREATED).body(jsonString);
+        NoCRequestDetails result = verifyNoCAnswersService.verifyNoCAnswers(verifyNoCAnswersRequest);
+        VerifyNoCAnswersResponse verifyNoCAnswersResponse = result.toVerifyNoCAnswersResponse(
+            VERIFY_NOC_ANSWERS_MESSAGE);
+        if (verifyNoCAnswersResponse != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(verifyNoCAnswersResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BadRequest("", "", "",
+                                                                                     new String[]{}));
+        }
     }
 }

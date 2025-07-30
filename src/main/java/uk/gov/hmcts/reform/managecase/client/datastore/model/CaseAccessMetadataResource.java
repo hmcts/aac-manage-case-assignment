0799e502335d413ca1f0269e9c33b6bf610083da
patelila <ila.patel@hmcts.net>
2025-07-30 13:22:35 +0100

    replace @ApiModel with @Schema

--- src/main/java/uk/gov/hmcts/reform/managecase/client/datastore/model/CaseAccessMetadataResource.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/client/datastore/model/CaseAccessMetadataResource.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.managecase.client.datastore.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.GrantType;
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Data
 @Builder
-@ApiModel(description = "Case Access Metadata of a Case Type")
+@Schema(description = "Case Access Metadata of a Case Type")
 public class CaseAccessMetadataResource {
 
     @JsonProperty("accessGrants")

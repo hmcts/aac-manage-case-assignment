e071280e231eee86c01fa695b93f06c5deb564ff
patelila <ila.patel@hmcts.net>
2026-03-26 12:23:08 +0000

    Align JWT issuer wiring and docs with standard issuer pattern

--- src/main/java/uk/gov/hmcts/reform/managecase/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/managecase/config/SecurityConfiguration.java
@@ -83,7 +83,7 @@ public class SecurityConfiguration {
     @SuppressWarnings("PMD")
     JwtDecoder jwtDecoder() {
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
-        // We are using issuerOverride instead of issuerUri as SIDAM has the wrong issuer at the moment
+        // See docs/security/jwt-issuer-validation.md for issuer-uri discovery and oidc.issuer enforcement.
         OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
         OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerOverride);
         OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);

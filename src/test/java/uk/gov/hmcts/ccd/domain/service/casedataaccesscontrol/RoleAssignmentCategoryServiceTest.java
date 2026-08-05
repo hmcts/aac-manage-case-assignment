49894567ac860056a47498d670c0879170c15bdc
JamesCatto <James.Catto@HMCTS.NET>
2026-08-05 17:37:12 +0100

    pofcc-369 - allow users with AM roles of solicitor and pui-case-manager to be granted the PROFESSIONAL role category

--- src/test/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentCategoryServiceTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentCategoryServiceTest.java
@@ -38,7 +38,29 @@ class RoleAssignmentCategoryServiceTest {
     class GetRoleCategory {
 
         @Test
-        void shouldGetRoleCategoryForSolicitorUser() {
+        void shouldGetRoleCategoryForUserWithPuiCaseManagerRole() {
+
+            given(caseAssignmentService.getAssigneeRoles(USER_ID))
+                .willReturn(asList("caseworker", "pui-case-manager"));
+
+            RoleCategory roleCategory = roleAssignmentCategoryService.getRoleCategory(USER_ID);
+
+            assertThat(roleCategory, is(PROFESSIONAL));
+        }
+
+        @Test
+        void shouldGetRoleCategoryForUserWithSolicitorRole() {
+
+            given(caseAssignmentService.getAssigneeRoles(USER_ID))
+                .willReturn(asList("caseworker", "solicitor"));
+
+            RoleCategory roleCategory = roleAssignmentCategoryService.getRoleCategory(USER_ID);
+
+            assertThat(roleCategory, is(PROFESSIONAL));
+        }
+
+        @Test
+        void shouldGetRoleCategoryForUserWithRoleWithSolicitorSuffix() {
 
             given(caseAssignmentService.getAssigneeRoles(USER_ID))
                 .willReturn(asList("caseworker", "caseworker-autotest1-solicitor"));
